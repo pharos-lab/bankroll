00e8f0dfbc27b7d78cf5758a63ce753e6694bf8b
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-10-02 14:07:52 +0200

    add heading and add button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <Navbar/>
-  <main class="grow flex">
+  <main class="grow flex text-cyan-900">
     <Sidebar />
     <section class="grow h-full overflow-auto bg-slate-50 p-8">
+      <h1 class="font-bold mb-8 flex justify-between">
+       <span class="text-2xl">POKER</span>
+       <button class="px-3 py-2 border-2 border-cyan-600 bg-cyan-600 rounded hover:bg-white hover:text-cyan-600 transition-colors text-white flex items-center gap-2">
+        <PlusIcon class="size-5"></PlusIcon>
+        New
+      </button>
+      </h1>
       <div class="charts">
-        <Chart :data="data" class="max-h-56" no-grid-x/>
+        <Chart :data="data" class="max-h-56" no-grid-x no-legend/>
       </div>
     </section>
   </main>
@@ -14,6 +21,7 @@
 import Navbar from './components/Navbar.vue';
 import Sidebar from './components/Sidebar.vue'
 import Chart from './components/Chart.vue'
+import { PlusIcon } from '@heroicons/vue/24/solid'
 
 const rawData = [
     { year: 2010, count: 10 },
