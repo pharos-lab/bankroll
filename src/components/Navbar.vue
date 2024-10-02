c9a8e9d83a56113dfd0984d838c088d316c26d16
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-10-02 09:45:52 +0200

    fix: change color to be all cyan for all components

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="bg-gradient-to-r from-cyan-800 to-cyan-600 text-white text-slate-50 p-3 font-semibold">
+  <header class="bg-gradient-to-r from-cyan-800 to-cyan-600 text-white p-3 font-semibold">
     <div class="container mx-auto flex items-center gap-8">
       <div class="brand">BANKROLL</div>
       <nav class="grow"></nav>
@@ -8,11 +8,11 @@
           <div class="avatr w-7 h-7 bg-red-500 rounded-full" @click="showDropdown = ! showDropdown">
             
           </div>
-          <div class="dropdown absolute top-12 -right-2 bg-sky-500 flex flex-col gap-2 px-2 py-2 rounded min-w-32" v-show="showDropdown">
-            <a href="ok" class="p-2 hover:bg-sky-600 rounded">Profile</a>
-            <a href="ok" class="p-2 hover:bg-sky-600 rounded">Profile</a>
-            <a href="ok" class="p-2 hover:bg-sky-600 rounded">Profile</a>
-            <a href="ok" class="p-2 hover:bg-sky-600 rounded">Profile</a>
+          <div class="dropdown absolute top-12 -right-2 bg-cyan-600 flex flex-col gap-2 px-2 py-2 rounded min-w-32 text-slate-100" v-show="showDropdown">
+            <a href="ok" class="p-2 hover:bg-cyan-700 hover:text-white rounded">Profile</a>
+            <a href="ok" class="p-2 hover:bg-cyan-700 hover:text-white rounded">Profile</a>
+            <a href="ok" class="p-2 hover:bg-cyan-700 hover:text-white rounded">Profile</a>
+            <a href="ok" class="p-2 hover:bg-cyan-700 hover:text-white rounded">Profile</a>
           </div>
         </div>
       </div>
